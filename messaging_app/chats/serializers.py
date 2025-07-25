f447a1cb03b78cfa53b59eacee3e023fbca0e679
DannyNak2 <danielnakachew1@gmail.com>
2025-07-25 15:05:00 +0300

    ceated seriallizer and urls in chats and edited the urls file in messaging app

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'role']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'conversation', 'message_body', 'sent_at']
+        read_only_fields = ['id', 'sent_at', 'sender']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'created_at', 'messages']
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically add current user to participants
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+        conversation.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
