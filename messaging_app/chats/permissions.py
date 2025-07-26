488055f342596ffca1dd8d27fd0c68883a602753
DannyNak2 <danielnakachew1@gmail.com>
2025-07-26 13:16:45 +0300

    updating permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,15 +1,30 @@
+from .models import User, Conversation, Message, Property, Booking, Payment
+from rest_framework import authentication
 from rest_framework import permissions
-from .models import Conversation
 
 class IsParticipantOfConversation(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Applies for Message or Conversation object
-        user = request.user
-        if hasattr(obj, 'participants'):
-            return user in obj.participants.all()
-        elif hasattr(obj, 'conversation'):
-            return user in obj.conversation.participants.all()
-        return False
+    """
+    Custom permission to only allow participants of a conversation
+    or sender/recipient of a message to access them.
+    """
 
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        # For Conversation objects
+        if hasattr(obj, 'participants'):
+            if request.method in permissions.SAFE_METHODS:
+                return user in obj.participants.all() and user.is_authenticated
+            # Only allow PUT/PATCH/DELETE for participants
+            if request.method in ['PUT', 'PATCH', 'DELETE']:
+                return user in obj.participants.all() and user.is_authenticated
+
+        # For Message objects
+        if hasattr(obj, 'sender'):
+            if request.method in permissions.SAFE_METHODS:
+                return obj.sender == user or user in obj.conversation.participants.all() and user.is_authenticated
+            # Only allow PUT/PATCH/DELETE for the sender
+            if request.method in ['PUT', 'PATCH', 'DELETE']:
+                return obj.sender == user and user.is_authenticated
+
+        return False
