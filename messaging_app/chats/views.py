623ac2056f2f8cec4a36e26f923dd2ec919e88ff
DannyNak2 <danielnakachew1@gmail.com>
2025-07-26 13:19:05 +0300

    updating chats view file

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,46 +1,66 @@
-from rest_framework import viewsets, status, filters
+from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import Conversation, Message
-from .serializers import ConversationSerializer, MessageSerializer
-from django.contrib.auth import get_user_model
-from .permissions import IsParticipantOfConversation
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status, filters
+from rest_framework.permissions import IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
-from .pagination import MessagePagination
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+from .auth import IsAuthenticated
 from .filters import MessageFilter
+from .pagination import PageNumberPagination
 
-User = get_user_model()
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['username', 'email']
+    ordering_fields = ['created_at']
+
+    def create(self, request, *args, **kwargs):
+        self.permission_classes = [IsAdminUser, IsAuthenticated]
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "User created successfully", "data": response.data},
+            status=status.HTTP_201_CREATED
+        )
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['created_at']
-
-    def perform_create(self, serializer):
-        conversation = serializer.save()
-        participant_ids = self.request.data.get('participant_ids', [])
-        if participant_ids:
-            users = User.objects.filter(user_id__in=participant_ids)
-            conversation.participants.set(users)
-        conversation.participants.add(self.request.user)
-        conversation.save()
-
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['participants']
+    #permission_classes = [IsParticipantOfConversation]
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['sent_at']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = MessageFilter
-    pagination_class = MessagePagination
+    search_fields = ['message_body']
+    ordering_fields = ['sent_at']
+    pagination_class = PageNumberPagination
 
-    def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "Message created successfully", "data": response.data},
+            status=status.HTTP_201_CREATED
+        )
+        return Response(
+            {"message": "Permission denied"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    def list(self, request, *args, **kwargs):
+        """
+        List messages in a conversation.
+        Only participants of the conversation can view the messages.
+        """
+        queryset = Message.objects.filter(conversation_id=request.query_params.get('conversation_id'))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-class MessageViewSet(viewsets.ModelViewSet):
 
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+# Create your views here.
